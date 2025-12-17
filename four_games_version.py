c000f630b597005227986e7bed881a24c34e02af
22weishan <125321920@qq.com>
2025-12-16 22:07:55 -0500

    Replace experimental rerun with standard rerun

--- four_games_version.py
+++ four_games_version.py
@@ -510,7 +510,7 @@ if st.session_state.game_started and st.session_state.game_mode == "Listen & Cho
             else:
                 st.error(f"Wrong. The correct answer was **{current_word}**.")
             st.session_state.listen_index += 1
-            st.experimental_rerun() 
+            st.rerun()
             
 
     else:
